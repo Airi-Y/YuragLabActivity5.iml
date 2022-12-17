de518915871394eac341b724464695660ae20090
Yurag <arnieyurag@gmail.com>
2022-12-17 10:53:52 +0800

    Add FoodOrderingSystem main function

--- src/FoodOrdering/FoodOrderingSystem.java
+++ src/FoodOrdering/FoodOrderingSystem.java
@@ -1,10 +1,20 @@
 package FoodOrdering;
 
+import SimpleCalc.SimpleCalculator;
+
 import javax.swing.*;
 
-public class FoodOrderingSystem {
+public class FoodOrderingSystem extends JFrame{
     private JPanel panel1;
     private JCheckBox pizzaCheckBox;
     private JRadioButton noneRadioButton;
     private JButton orderButton;
+
+    public static void main(String[] args) {
+        FoodOrderingSystem app = new FoodOrderingSystem();
+        app.setContentPane(app.panel1);
+        app.setSize(600, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
