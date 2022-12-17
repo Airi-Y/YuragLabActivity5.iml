e7a54d87a7de0d6d282e72fdf1fd8d783fb5b607
Yurag <arnieyurag@gmail.com>
2022-12-17 10:51:30 +0800

    Add SimpleCalculator main function

--- src/SimpleCalc/SimpleCalculator.java
+++ src/SimpleCalc/SimpleCalculator.java
@@ -1,10 +1,20 @@
 package SimpleCalc;
 
+import LeapYear.LeapYearChecker;
+
 import javax.swing.*;
 
-public class SimpleCalculator {
+public class SimpleCalculator extends JFrame{
     private JPanel panel1;
     private JTextField textField1;
     private JComboBox comboBox1;
     private JButton computeResultButton;
+
+    public static void main(String[] args) {
+        SimpleCalculator app = new SimpleCalculator();
+        app.setContentPane(app.panel1);
+        app.setSize(600, 300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
