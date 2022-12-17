15a1a50ec9f8e51b09611d1fd9711f8e73d9c085
Yurag <arnieyurag@gmail.com>
2022-12-17 12:30:15 +0800

    Complete LeapYearChecker

--- src/LeapYear/LeapYearChecker.java
+++ src/LeapYear/LeapYearChecker.java
@@ -1,11 +1,43 @@
 package LeapYear;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearChecker extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public LeapYearChecker() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkYear();
+            }
+        });
+    }
+
+        public void checkYear() {
+                double yr = Double.parseDouble(tfYear.getText());
+                boolean leap;
+
+                if (yr % 4 == 0) {
+                    if (yr % 100 == 0) {
+                        leap = yr % 400 == 0;
+                    } else {
+                        leap = true;
+                    }
+                } else {
+                    leap = false;
+                }
+
+                if (leap) {
+                    JOptionPane.showMessageDialog(panel1, "Leap Year");
+                } else {
+                    JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+                }
+                tfYear.setText("");
+    }
 
     public static void main(String[] args) {
         LeapYearChecker app = new LeapYearChecker();
@@ -13,5 +45,6 @@ public class LeapYearChecker extends JFrame{
         app.setSize(350, 350);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+        app.setTitle("Leap Year Checker");
     }
 }
